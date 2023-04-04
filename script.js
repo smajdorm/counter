0589ab4eebbbc465894ba72f24d1382ded6b0587
Mariusz <smajdortech@gmail.com>
2023-04-04 20:21:51 +0200

    Counter value is now saved in local storage

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const counterEl = document.querySelector('.counter');
 const buttonEls = document.querySelectorAll('.button');
 const stepEl = document.querySelector('.step');
 
-let counter = 0;
+const COUNTER_LS_KEY = 'counter.counter';
+
+let counter = localStorage.getItem(COUNTER_LS_KEY) || 0;
 let step = 1;
 
 stepEl.addEventListener('input', () => {
@@ -23,6 +25,7 @@ function manipulateCounter(action) {
   else if (action === 'decrease') counter -= step;
   else counter = 0;
 
+  localStorage.setItem(COUNTER_LS_KEY, counter);
   counterEl.innerText = counter;
 }
 
